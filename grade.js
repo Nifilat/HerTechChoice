c14652ebaa8b8e50ba21c7d0fefb544f03b1926b
Nifilat <latunde.nifemi2016@gmail.com>
2022-02-26 22:05:02 +0100

    Create grade.js
    Backend Task 1

---
+++ grade.js
@@ -0,0 +1,21 @@
+var maths = 82;
+
+var english = 58;
+
+var biology = 64;
+
+var total = maths + english + biology;
+
+var Average = total / 3;
+
+console.log ("Your average score is " + Average);
+
+if (Average >= 70){
+ console.log("Your grade is A")
+}
+else if (Average <= 39){
+ console.log("Your grade is F")
+}
+else {
+ console.log("You passed the Course")
+};
