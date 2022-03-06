1b2c2eced2b69f6835140815053098853ed67980
Nifilat <latunde.nifemi2016@gmail.com>
2022-03-06 18:33:14 +0100

    Create bank-account.js
    Backend Task 2

---
+++ bank-account.js
@@ -0,0 +1,50 @@
+var account = {};
+function createAccount(name, age, address, balance) {
+ account.name = name;
+ account.age = age;
+ account.address = address;
+ account.balance = balance;
+ account.status = true;
+
+ console.log(account);
+
+}
+
+function depositAmount(amount) {
+
+ if (account.status == true) {
+   account.balance += amount;
+   console.log("Your account balance is " + account.balance);
+  
+ }
+ else {
+  console.log ("Unable to deposit")
+ }
+ 
+}
+
+function withdrawAmount(amount) {
+ if(account.status == true) {
+  if (account.balance >= amount) {
+   account.balance -= amount
+   console.log("Your account balance is " + account.balance);
+  }
+  else {
+   console.log("Insufficient balance")
+  }
+ }
+ else{
+  console.log("Unable to withdraw")
+ }
+}
+
+function changeStatus() {
+ var currentStatus = account.status;
+ account.status = !currentStatus;
+ console.log("Your account status is " + account.status )
+}
+
+createAccount("Latunde Oluwanifemi", "24", "CMF Hostel", 20000);
+
+changeStatus();
+withdrawAmount(12000);
