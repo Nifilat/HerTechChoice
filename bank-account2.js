083f258b9508d2adb9c1750dedbc515319b1c569
Nifilat <latunde.nifemi2016@gmail.com>
2022-03-12 16:10:12 +0100

    Create bank-account2.js
    Backend Task 3

---
+++ bank-account2.js
@@ -0,0 +1,28 @@
+let accounts = {account1, account2, account3, account4};
+
+
+function createAccount(account) {
+ account.balance = balance;
+ account.username = username;
+
+}
+
+function getAccount(username) {
+
+}
+
+function withdraw(username, amount) {
+
+}
+
+function deposit(username, amount) {
+
+}
+
+function getBalance(username) {
+
+}
+
+function getBankBalance() {
+
+}
