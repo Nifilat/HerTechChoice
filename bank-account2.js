50c9f06f7d1ed3ffb663ce452e32b651930ed262
Nifilat <latunde.nifemi2016@gmail.com>
2022-03-13 22:32:17 +0100

    Update bank-account2.js

--- bank-account2.js
+++ bank-account2.js
@@ -1,28 +1,78 @@
-let accounts = {account1, account2, account3, account4};
+const bankAccounts = []
 
-
-function createAccount(account) {
- account.balance = balance;
- account.username = username;
-
+function createAccount(username, balance) {
+    account = {
+        username: username.toLowerCase(),
+        balance: balance
+    }
+    bankAccounts.push(account)
 }
 
 function getAccount(username) {
-
+    //Loops through the bankAccounts array and check for an account that matches the specified username
+    for (account of bankAccounts) {
+        if (account.username == username.toLowerCase()){
+            return account
+        }
+    }
+    console.log("Account Not Found")
+    return null
 }
 
-function withdraw(username, amount) {
-
+function withdraw(username, amount){
+    for (i=0; i<bankAccounts.length; i++){
+        account = bankAccounts[i]
+        if (account.username == username.toLowerCase()){
+            if (account.balance >= amount) {
+                account.balance -= amount
+                bankAccounts[i] = account
+                return account
+            }
+            console.log("Insufficient Funds")
+            return account
+        }
+    }
+    console.log("Account Not Found")
+    return null
 }
 
-function deposit(username, amount) {
-
+function deposit(username, amount){
+    for (i=0; i<bankAccounts.length; i++){
+        account = bankAccounts[i]
+        if (account.username == username.toLowerCase()){
+            account.balance += amount
+            bankAccounts[i] = account
+            return account
+        }
+    }
+    console.log("Account Not Found")
+    return null
 }
 
 function getBalance(username) {
-
+    for (account of bankAccounts) {
+        if (account.username == username.toLowerCase()){
+            return account.balance
+        }
+    }
+    console.log("Account Not Found")
+    return null
 }
 
 function getBankBalance() {
-
+    total = 0
+    for (account of bankAccounts) {
+        total += account.balance
+    }
+    return total
 }
+
+createAccount("Nife", 2000)
+createAccount("Bolanle", 5000)
+createAccount("Bidemi", 0)
+console.log(withdraw("Matthew", 3000))
+console.log(withdraw("Nife", 2000.50))
+console.log(deposit("Nife", 2000))
+console.log(getAccount("Nife"))
+console.log(getBalance("NIFE"))
+console.log(getBankBalance())
