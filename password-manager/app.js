f97bef8f8855c72936c3170bbd0a61f02c30e20e
Nifilat <latunde.nifemi2016@gmail.com>
2022-03-17 15:41:03 +0100

    ..

--- password-manager/app.js
+++ password-manager/app.js
@@ -1,5 +1,6 @@
 const {getItem, setItem} = require("node-persist")
 const storage = require("node-persist");
+const argv = require("yargs").argv;
 
 console.log("Starting password manager!!!")
 
@@ -37,4 +38,18 @@ async function getAccount(name) {
 
 }
 
-getAccount("Facebook")
+if (argv._[0] == "create") {
+ var account = {
+  "name": argv.name,
+  "username": argv.username,
+  "password": argv.password
+ }
+ createAccount(account);
+}
+else if (argv._[0] == 'get') {
+ getAccount(argv.name)
+
+}
+else {
+ console.log("Please enter a command")
+}
--- password-manager/argv.js
+++ password-manager/argv.js
@@ -1,3 +1,11 @@
 var argv = require("yargs").argv
 
-console.log(argv);
+console.log(argv)
+
+if (argv._[0] == "greet" && typeof argv.name != "undefined") {
+ console.log("Hello " + argv.name);
+}
+
+else {
+ console.log("Hello world");
+}
