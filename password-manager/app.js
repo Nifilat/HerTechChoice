acb2067339b0f9902aed2f1a5372491110f94b6e
Nifilat <latunde.nifemi2016@gmail.com>
2022-03-17 11:02:21 +0100

    update

--- password-manager/app.js
+++ password-manager/app.js
@@ -1,1 +1,16 @@
-console.log('Starting Password Manager!!!!!')
+const storage = require("node-persist")
+
+
+console.log('Starting Password Manager!!!!!');
+
+async function store() {
+ await storage.init();
+ var name = await storage.getItem("name")
+ var realname = await storage.getItem("realname")
+
+ console.log(name +" " + realname)
+ 
+ 
+}
+
+store();
